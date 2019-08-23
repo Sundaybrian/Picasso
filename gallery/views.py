e42366bfda1588a31148c3e181c0424c04a46e10
Sunday Brian <brians931@gmail.com>
2019-08-23 16:36:12 +0300

    add a welcome page html

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 # Create your views here.
 
 def welcome(request):
     return HttpResponse('Welcome to picasso mademioselle')
+
