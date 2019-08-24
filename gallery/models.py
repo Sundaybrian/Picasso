c75bce82c1a0beb9552b7c630c1c23bdec4d7e76
Sunday Brian <brians931@gmail.com>
2019-08-24 15:06:02 +0300

    add the image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Location(models.Model):
@@ -13,3 +14,15 @@ class Category:
 
     def __str__(self):
         return self.category_name
+
+class Image(models.Model):
+    img_name=models.CharField(max_length=100)
+    img_desc=models.TextField()   
+    img_loc=models.ForeignKey(Location)
+    img_category=models.ForeignKey(Category)
+    pub_date=models.DateTimeField(auto_now_add=True)
+    last_updated=models.DateTimeField(auto_now=True)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+       
+       def __str__(self):
+           return f'Image{self.img_name}-{self.img_loc}-{self.img_category}'
