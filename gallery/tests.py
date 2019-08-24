fd032e92255ee2a0cf7ebcaf635260e8bf651117
Sunday Brian <brians931@gmail.com>
2019-08-24 15:22:32 +0300

    add the setUp test and instance test for the category

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Category,Image,Location
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+
+    #set up method
+    def setUp(self):
+        self.food=Category(category_name='Food')
+
+    #Testing Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.food,Category))    
