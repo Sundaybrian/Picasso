0bd9ead16a0256664c09b19efc87a104ee69ec93
Sunday Brian <brians931@gmail.com>
2019-08-24 19:39:36 +0300

    add the search_results view function that fetches photos by category

--- gallery/views.py
+++ gallery/views.py
@@ -17,3 +17,22 @@ def picassoHome(request):
     }
 
     return render(request,'all-canvas/picasso_home.html',context)
+
+def search_results(request):
+    if 'photo' in request.GET and request.GET['photo']:
+        search_term=request.GET.get('photo')
+        searched_photos=Image.search_by_category(search_term)
+        
+        context={
+        'message':f"{search_term}",
+        'photos':searched_photos
+        }
+
+        return render(request,'all-canvas/search.html',context)
+
+    else:
+         context={
+        'message':"You haven't searched for any term"
+        }
+
+        return render(request,'all-canvas/search.html',context)    
