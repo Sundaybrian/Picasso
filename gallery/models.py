305e65571ccf2f6bcb8fd24419174f20a4436ac6
Sunday Brian <brians931@gmail.com>
2019-08-24 14:41:47 +0300

    create the loaction and category models

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    loc_name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.loc_name
+
+
+class Category:
+    category_name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
