82e38ff8f2c90a66fc796b52a4af934e1d7142f6
Sunday Brian <brians931@gmail.com>
2019-08-24 16:32:00 +0300

    update the admin.py by adding the ImageAdmin class that will allow ordering of many fields for the tags

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Location,Category,Image
+from .models import Location,Category,Image,tags
 
 # Register your models here.
 
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal=('tags',)
+
 admin.site.register(Location)
 admin.site.register(Category)
-admin.site.register(Image)
+admin.site.register(Image,ImageAdmin)
+admin.site.register(tags)
 
