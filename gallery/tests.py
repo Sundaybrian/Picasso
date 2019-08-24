d366b20243d460f49ce7c0ee13308d1283375b30
Sunday Brian <brians931@gmail.com>
2019-08-24 15:25:49 +0300

    add the save test method for the category

--- gallery/tests.py
+++ gallery/tests.py
@@ -10,4 +10,10 @@ class CategoryTestClass(TestCase):
 
     #Testing Instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.food,Category))    
+        self.assertTrue(isinstance(self.food,Category))   
+
+     #Testing save method
+    def test_save_method(self):
+        self.food.save_category()
+        categories=Category.objects.all()
+        self.assertTrue(len(categories)>0)    
