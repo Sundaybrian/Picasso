ea805abd321a6f8ab3f21ae80244b1a900e65d23
Sunday Brian <brians931@gmail.com>
2019-08-24 19:26:43 +0300

    updated the gallery url to include the search results view

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.welcome,name='welcome'),
-    url(r'^home/$',views.picassoHome,name='picassoHome')
+    url(r'^home/$',views.picassoHome,name='picassoHome'),
+    url(r'^search/$',views.search_results,name='search_results')
 ]
