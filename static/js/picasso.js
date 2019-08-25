854c7c4b266735beac6645968df585f24285bcd8
Sunday Brian <brians931@gmail.com>
2019-08-25 16:18:19 +0300

    added the imagesloaded library to style render the fird items differently

--- static/js/picasso.js
+++ static/js/picasso.js
@@ -1,9 +1,19 @@
 // masonry
-$('.grid').masonry({
-    itemSelector:'.grid-item',
-    columnWidth:110,
-    fitWidth:true,
-    gutter:0
-})
+// $('.grid').masonry({
+//     itemSelector:'.grid-item',
+//     columnWidth:110,
+//     fitWidth:true,
+//     gutter:0
+// })
+
+
+// init Masonry after all images have loaded
+var $grid = $('.grid').imagesLoaded( function() {
+    $grid.masonry({
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      columnWidth: '.grid-sizer'
+    }); 
+  });
 
 
